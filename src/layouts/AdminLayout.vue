86390212aa010d96377c991a82f32523593a13d9
a31361548 <a31361548@gmail.com>
2024-01-18 16:37:22 +0800

    first commit

---
+++ src/layouts/AdminLayout.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+VNavigationDrawer(permanent)
+  VList
+    VListItem(
+      v-for="item in navItems" :key="item.to"
+      :to="item.to" :title="item.text" :prepend-icon="item.icon"
+    )
+VMain
+  RouterView
+</template>
+
+<script setup>
+const navItems = [
+  { to: '/admin/products', text: '商品管理', icon: 'mdi-shopping' },
+  { to: '/admin/orders', text: '訂單管理', icon: 'mdi-list-box' },
+  { to: '/', text: '回首頁', icon: 'mdi-home' }
+]
+</script>
